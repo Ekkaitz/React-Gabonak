570753b428cd9f0690445c29606d322279e7d573
ekaru <erubioel22wg@ikzubirimanteo.com>
2024-01-10 19:25:50 +0100

    Bukatuta

--- ariketa/src/components/udalherria.jsx
+++ ariketa/src/components/udalherria.jsx
@@ -1,80 +1,99 @@
-import React from 'react'
-import Datuak from './datuak'
+import React, { useState, useEffect } from 'react';
+import Datuak from './datuak';
 
-const udalherria = ({selectedProbintzia}) => {
-  let arrayInguruneak
+const Udalherria = ({ selectedProbintzia }) => {
+  const [arrayInguruneak, setArrayInguruneak] = useState([]);
+  const [udalherria, setUdalherria] = useState('');
+  const [ingurunea, setIngurunea] = useState('');
+  const [datuak, setDatuak] = useState({
+    izena: '',
+    localidad: '',
+    mota: '',
+    turismos: '',
+    deskripzioa: '',
+  });
 
-  function ajax() {
-    const http = new XMLHttpRequest;
-    var inguruneak;
-    
-    let url = 'http://127.0.0.1/server/espacios.php';
-    http.open("GET", url, true);
-    http.send();
-  
-    http.onreadystatechange = function(){
-      if (http.readyState == 4 && http.status == 200) {
-        inguruneak=http.response;
-        let json=JSON.parse(inguruneak);
-        
-        arrayInguruneak=json.item;
-
-        let options=""
-
-        for (let i = 0; i < arrayInguruneak.length; i++) {
-          if (arrayInguruneak[i].territory.includes(selectedProbintzia)) {
-            options+="<option value='"+arrayInguruneak[i].municipality+"'>"+arrayInguruneak[i].municipality+"</option>";
-          }
-        }
-
-        var udalherria =document.getElementById("udalherria").value;
-        var options2="";
-
-        for (let i = 0; i < arrayInguruneak.length; i++) {
-          if (arrayInguruneak[i].municipality.includes(udalherria)) {
-            options2+="<option value='"+arrayInguruneak[i].documentName+"'>"+arrayInguruneak[i].documentName+"</option>";
-          }          
-        }
-
-        document.getElementById("udalherria").innerHTML=options
-        document.getElementById("ingurunea").innerHTML=options2
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1/server/espacios.php');
+        const data = await response.json();
+        setArrayInguruneak(data.item);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleUdalherriaChange = (event) => {
+    const selectedUdalherria = event.target.value;
+    setUdalherria(selectedUdalherria);
+    setIngurunea(''); // Limpiar las opciones de ingurunea al cambiar udalherria
+
+    // Filtrar y actualizar el array de opciones de ingurunea
+    const filteredOptions = arrayInguruneak
+      .filter((item) => item.territory.includes(selectedProbintzia) && item.municipality === selectedUdalherria)
+      .map((item) => item.documentName);
+
+    setInguruneaOptions(filteredOptions);
+  };
+
+  const handleInguruneaChange = (event) => {
+    const selectedIngurunea = event.target.value;
+    setIngurunea(selectedIngurunea);
+
+    // Encontrar los datos correspondientes al seleccionar ingurunea
+    const selectedDatuak = arrayInguruneak.find((item) => item.documentName === selectedIngurunea);
+
+    if (selectedDatuak) {
+      setDatuak({
+        izena: selectedDatuak.documentName,
+        localidad: selectedDatuak.templateType,
+        mota: selectedDatuak.marks,
+        turismos: selectedDatuak.turismDescription,
+        deskripzioa: selectedDatuak.turismDescription,
+      });
     }
-  }
+  };
 
-  function kargatu() {
-   for (let i = 0; i < arrayInguruneak.length; i++) {
-    if (arrayInguruneak[i].documentName==document.getElementById("ingurunea").value) {
-      var izena =arrayInguruneak.documentName;
-      var mota=arrayInguruneak.templateType;
-      var turismos=arrayInguruneak.marks;
-      var deskripzioa=arrayInguruneak.turismDescription;
+  const setInguruneaOptions = (options) => {
+    const inguruneaSelect = document.getElementById('ingurunea');
+    inguruneaSelect.innerHTML = ''; // Limpiar las opciones antes de añadir nuevas
 
-      //aqui habia que pasar las variables al componente DATUAK de alguna manera
-    }    
-   }
-  }
-  
-  ajax();
-  
+    // Añadir las nuevas opciones al select
+    options.forEach((option) => {
+      const newOption = document.createElement('option');
+      newOption.value = option;
+      newOption.text = option;
+      inguruneaSelect.add(newOption);
+    });
+  };
+
   return (
     <div>
       <p>
         udalherria
-        <select name="udalherria" id="udalherria" onChange={ajax}>
+        <select name="udalherria" id="udalherria" value={udalherria} onChange={handleUdalherriaChange}>
           <option value="">-udalherria-</option>
+          {arrayInguruneak
+            .filter((item) => item.territory.includes(selectedProbintzia))
+            .map((item) => (
+              <option key={item.municipality} value={item.municipality}>
+                {item.municipality}
+              </option>
+            ))}
         </select>
         Entorno natural
-
-        <select name="ingurunea" id="ingurunea" onChange={kargatu}>
+        <select name="ingurunea" id="ingurunea" value={ingurunea} onChange={handleInguruneaChange}>
           <option value="">-ingurunea-</option>
         </select>
       </p>
 
-      <Datuak izena="" localidad="" mota="" turismos="" deskripzioa=""  />
+      <Datuak {...datuak} />
     </div>
-  )
-}
+  );
+};
 
-export default udalherria
+export default Udalherria;
